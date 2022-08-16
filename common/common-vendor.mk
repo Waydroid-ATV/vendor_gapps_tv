2c7f939837daadbfc353dccf88c30fcadd696aff
Nolen Johnson <johnsonnolen@gmail.com>
2022-08-16 14:28:34 -0400

    Update to newer Google apps
    * And add a few missing bits.

--- common/common-vendor.mk
+++ common/common-vendor.mk
@@ -6,24 +6,34 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/gapps_tv/common
 
 PRODUCT_COPY_FILES += \
+    vendor/gapps_tv/common/proprietary/product/etc/default-permissions/default-atv-permissions.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/default-permissions/default-atv-permissions.xml \
     vendor/gapps_tv/common/proprietary/product/etc/permissions/privapp-permissions-atv-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-atv-product.xml \
-    vendor/gapps_tv/common/proprietary/product/etc/permissions/privapp-permissions-google-p.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-google-p.xml \
+    vendor/gapps_tv/common/proprietary/product/etc/permissions/privapp-permissions-google-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-google-product.xml \
+    vendor/gapps_tv/common/proprietary/product/etc/permissions/split-permissions-google.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/split-permissions-google.xml \
     vendor/gapps_tv/common/proprietary/product/etc/sysconfig/google-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/google-hiddenapi-package-whitelist.xml \
+    vendor/gapps_tv/common/proprietary/product/etc/sysconfig/google-staged-installer-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/google-staged-installer-whitelist.xml \
     vendor/gapps_tv/common/proprietary/product/etc/sysconfig/google.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/google.xml \
     vendor/gapps_tv/common/proprietary/product/etc/sysconfig/google_atv.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/google_atv.xml \
-    vendor/gapps_tv/common/proprietary/system_ext/etc/permissions/privapp-permissions-google-se.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-google-se.xml
+    vendor/gapps_tv/common/proprietary/system/etc/permissions/privapp-permissions-google-system.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-google-system.xml \
+    vendor/gapps_tv/common/proprietary/system_ext/etc/permissions/privapp-permissions-google-system_ext.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-google-system_ext.xml
 
 PRODUCT_PACKAGES += \
+    GoogleExtShared \
+    GooglePackageInstaller \
     GoogleCalendarSyncAdapter \
+    GoogleTTS \
     talkback \
+    AtvAxel \
     AtvRemoteService \
     Backdrop \
+    GoogleOneTimeInitializer \
     Katniss \
     SetupWraithPrebuilt \
-    SssAuthbridgePrebuilt \
+    TVLauncher \
+    TVRecommendations \
     Tubesky \
     GoogleFeedback \
-    GoogleOneTimeInitializer \
+    GooglePartnerSetup \
     GoogleServicesFramework
 
 PRODUCT_SOONG_NAMESPACES += vendor/gapps_tv/overlay
