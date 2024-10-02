01a89d88caaa2d36fd77601afb79baff5c821c24
supechicken <me@supechicken666.dev>
2024-10-02 16:23:08 +0000

    Add x86_64 support
    Signed-off-by: supechicken <me@supechicken666.dev>

--- build/main.mk
+++ build/main.mk
@@ -17,6 +17,10 @@ gapps_tv_arm64:
 	@echo "Compiling GApps with GMS launcher for arm64..."
 	@bash $(BUILD_GAPPS) arm64 full 2>&1
 
+gapps_tv_x86_64:
+	@echo "Compiling GApps with GMS launcher for x86_64..."
+	@bash $(BUILD_GAPPS) x86_64 full 2>&1
+
 gapps_tv_arm_minimal:
 	@echo "Compiling GApps without GMS Launcher for arm..."
 	@bash $(BUILD_GAPPS) arm minimal 2>&1
